564f97cd0f7d3885f5d6e1b0aab6adfa4906d608
Taehee Jeong <wwee3631@gmail.com>
2015-03-21 09:19:22 +0900

    [20150321] Shuffle implementation

---
+++ js/wordnote.js
@@ -0,0 +1,11 @@
+array.prototype.shuffle = function() {
+    var size = this.length;
+    for (var i = 0; i < size; i++) {
+        var j = Math.floor(Math.random() * size);
+        if (i !== j) {
+            var tmp = this[i];
+            this[i] = this[j];
+            this[j] = tmp;
+        }
+    }
+};
